dd0efb99c8f20885fad0c5450cd1b96185a7f033
Federico González <federico1818@gmail.com>
2026-01-05 07:41:20 +0000

    set page in nav

--- src/app/game/components/nav/nav.ts
+++ src/app/game/components/nav/nav.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
-import { RouterLink, RouterLinkActive } from '@angular/router'
+import { RouterLink, RouterLinkActive, NavigationEnd, Router, Event } from '@angular/router'
+import { filter } from 'rxjs'
 
 @Component({
     selector: 'app-nav',
@@ -9,5 +10,13 @@ import { RouterLink, RouterLinkActive } from '@angular/router'
 })
 
 export class Nav {
+    public page: string = ''
 
+    constructor(private router: Router) {
+        this.router.events
+            .pipe(filter(e => e instanceof NavigationEnd))
+            .subscribe((event: NavigationEnd) => {
+                this.page = event.urlAfterRedirects.split('/')[2]
+            })
+    }
 }
