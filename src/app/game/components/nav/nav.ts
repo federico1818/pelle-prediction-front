bc3fc8441cf73e25438e64a94166bfdc33b64c46
Federico González <federico1818@gmail.com>
2026-01-05 09:07:49 +0000

    change item active without animation

--- src/app/game/components/nav/nav.ts
+++ src/app/game/components/nav/nav.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core'
-import { RouterLink, RouterLinkActive, NavigationEnd, Router, Event } from '@angular/router'
+import { Component, signal, WritableSignal } from '@angular/core'
+import { NgClass } from '@angular/common'
+import { RouterLink, RouterLinkActive, Router, Event, NavigationStart } from '@angular/router'
 import { filter } from 'rxjs'
 
 @Component({
     selector: 'app-nav',
-    imports: [RouterLink, RouterLinkActive],
+    imports: [
+        NgClass,
+        RouterLink,
+        RouterLinkActive
+    ],
     templateUrl: './nav.html',
     styleUrl: './nav.css',
 })
 
 export class Nav {
-    public page: string = ''
+    public page: WritableSignal<string> = signal('')
 
     constructor(private router: Router) {
         this.router.events
-            .pipe(filter(e => e instanceof NavigationEnd))
-            .subscribe((event: NavigationEnd) => {
-                this.page = event.urlAfterRedirects.split('/')[2]
+            .pipe(filter((e: Event) => e instanceof NavigationStart))
+            .subscribe((event: NavigationStart) => {
+                this.page.set(event.url.split('/')[2])
             })
     }
 }
