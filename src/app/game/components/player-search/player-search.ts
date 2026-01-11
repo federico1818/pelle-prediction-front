a7ece25de5fb9b391abc05628a292b4400dc4eb0
Federico González <federico1818@gmail.com>
2026-01-11 08:58:57 +0000

    player search created

---
+++ src/app/game/components/player-search/player-search.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-player-search',
+    imports: [],
+    templateUrl: './player-search.html',
+    styleUrl: './player-search.css',
+})
+
+export class PlayerSearch {
+
+}
--- src/app/game/pages/list/list.ts
+++ src/app/game/pages/list/list.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core'
 import { PlayerList } from '../../components/player-list/player-list'
+import { PlayerSearch } from '../../components/player-search/player-search'
 
 @Component({
     selector: 'app-list',
-    imports: [PlayerList],
+    imports: [
+        PlayerList,
+        PlayerSearch
+    ],
     templateUrl: './list.html',
     styleUrl: './list.css',
 })
