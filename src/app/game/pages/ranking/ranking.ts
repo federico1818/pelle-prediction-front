fa9bf18d5a05a521e58c883cc7a7fef3e1951caf
Federico González <federico1818@gmail.com>
2026-02-10 06:28:53 +0000

    add dialogue

--- src/app/game/pages/ranking/ranking.ts
+++ src/app/game/pages/ranking/ranking.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core'
+import { Component, inject, OnInit } from '@angular/core'
+import { DialogueService } from '../../../shared/services/dialogue-service'
+import { Dialogue } from '../../../shared/components/dialogue/dialogue'
 
 @Component({
     selector: 'app-ranking',
-    imports: [],
+    imports: [
+        Dialogue
+    ],
     templateUrl: './ranking.html',
     styleUrl: './ranking.css',
 })
 
-export class Ranking {
+export class Ranking implements OnInit {
+    private _dialogueService: DialogueService = inject(DialogueService)
 
+    public ngOnInit(): void {
+        this._dialogueService.setText('Scaloni está trabajando arduamente en el armado de la lista.')
+    }
 }
