1a94dcfee437fb3d6e9fbc28c604639cffba9d2d
Federico González <federico1818@gmail.com>
2026-06-07 09:52:35 +0000

    ranking scene

--- src/app/game/pages/ranking/ranking.ts
+++ src/app/game/pages/ranking/ranking.ts
@@ -1,19 +1,23 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core'
-import { DialogueText } from '../../../shared/components/dialogue-text/dialogue-text'
+import { Component } from '@angular/core'
+import { SceneComponent } from '../../../shared/components/scene/scene'
+import { Scene } from '../../../shared/models/scene'
 
 @Component({
     selector: 'app-ranking',
     imports: [
-        DialogueText
+        SceneComponent
     ],
     templateUrl: './ranking.html',
     styleUrl: './ranking.css',
 })
-
-export class Ranking implements AfterViewInit {
-    @ViewChild('dialogueRef') dialogueElement!: DialogueText
-
-    public ngAfterViewInit(): void {
-        //this.dialogueElement.write('Scaloni está trabajando arduamente en el desarrollo del sistema.')
+export class Ranking {
+    public rankingScene: Scene = {
+        title: 'Ranking no disponible',
+        dialogues: [
+            {
+                text: 'Scaloni está trabajando arduamente en el desarrollo del sistema.',
+                video: 'scaloni-rec-animated.mp4'
+            }
+        ]
     }
 }
