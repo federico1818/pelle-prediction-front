1b4d97282fa8b4a812c2ce034e10ebd358e66808
Federico González <federico1818@gmail.com>
2026-06-13 04:57:03 +0000

    show points

--- src/app/game/components/match-prediction-item/match-prediction-item.ts
+++ src/app/game/components/match-prediction-item/match-prediction-item.ts
@@ -7,17 +7,8 @@ import { Prediction } from '../../models/prediction'
     imports: [CommonModule],
     templateUrl: './match-prediction-item.html',
     styleUrl: './match-prediction-item.css',
-    host: {
-        'class': 'prediction-item-host'
-    }
 })
+
 export class MatchPredictionItem {
     public prediction = input.required<Prediction>()
-
-    public getPointsClass(points: number | null): string {
-        if (points === null) return 'points-null'
-        if (points === 3) return 'points-exact'
-        if (points === 1) return 'points-trend'
-        return 'points-none'
-    }
 }
--- src/app/game/models/prediction.ts
+++ src/app/game/models/prediction.ts
@@ -1,6 +1,8 @@
+import type { Points } from './points'
+
 export interface Prediction {
     nickname: string
     score_1: number
     score_2: number
-    points: number | null
+    points: Points | null
 }
