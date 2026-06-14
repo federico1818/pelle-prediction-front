5e78a9ebed3d1c75d2adf49914bcbf69689cea6a
Federico González <federico1818@gmail.com>
2026-06-14 06:13:33 +0000

    home maradona

--- src/app/game/pages/home/home.ts
+++ src/app/game/pages/home/home.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Scene } from '../../../shared/models/scene'
+import { SceneComponent } from '../../../shared/components/scene/scene'
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+    selector: 'app-home',
+    imports: [SceneComponent],
+    templateUrl: './home.html',
+    styleUrl: './home.css',
 })
-export class Home {
 
+export class Home {
+    public scene: Scene = {
+        title: '',
+        dialogues: [
+            {
+                text: '¡Que no se te escape la tortuga! Hacé tu pronóstico',
+                src: 'scenes/maradona.png',
+                type: 'rpg'
+            }
+        ]
+    }
 }
