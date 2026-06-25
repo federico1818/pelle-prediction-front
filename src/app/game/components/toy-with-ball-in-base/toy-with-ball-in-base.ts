6c47ef687cea2aa01f9967a4dbd69cebe866da6e
Federico González <federico1818@gmail.com>
2026-06-25 04:04:35 +0000

    add toy base image

--- src/app/game/components/toy-with-ball-in-base/toy-with-ball-in-base.ts
+++ src/app/game/components/toy-with-ball-in-base/toy-with-ball-in-base.ts
@@ -1,9 +1,13 @@
 import { Component, input } from '@angular/core'
 import { ToyWithBall } from '../toy-with-ball/toy-with-ball'
+import { ImageToyBase } from '../../images/image-toy-base/image-toy-base'
 
 @Component({
     selector: 'app-toy-with-ball-in-base',
-    imports: [ToyWithBall],
+    imports: [
+        ToyWithBall,
+        ImageToyBase,
+    ],
     templateUrl: './toy-with-ball-in-base.html',
     styleUrl: './toy-with-ball-in-base.css',
 })
---
+++ src/app/game/images/image-toy-base/image-toy-base.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-image-toy-base',
+    imports: [],
+    templateUrl: './image-toy-base.html',
+    styleUrl: './image-toy-base.css',
+})
+
+export class ImageToyBase {
+
+}
