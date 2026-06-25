f342dcf610caea799b5309a818a0d0824947bbbf
Federico González <federico1818@gmail.com>
2026-06-25 06:32:48 +0000

    podium color

--- src/app/game/components/podium/podium.ts
+++ src/app/game/components/podium/podium.ts
@@ -1,10 +1,14 @@
 import { Component, input } from '@angular/core'
 import { ToyWithBall } from '../toy-with-ball/toy-with-ball'
 import { Ranking } from '../../models/ranking'
+import { ImagePodium } from '../../images/image-podium/image-podium'
 
 @Component({
     selector: 'app-podium',
-    imports: [ToyWithBall],
+    imports: [
+        ToyWithBall,
+        ImagePodium
+    ],
     templateUrl: './podium.html',
     styleUrl: './podium.css',
 })
---
+++ src/app/game/images/image-podium/image-podium.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-image-podium',
+    imports: [],
+    templateUrl: './image-podium.html',
+    styleUrl: './image-podium.css',
+})
+
+export class ImagePodium {
+
+}
