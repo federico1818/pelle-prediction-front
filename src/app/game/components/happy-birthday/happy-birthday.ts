4d6ec416c090cfbde87d29a0fac120cb86f10eb9
Federico González <federico1818@gmail.com>
2026-07-08 02:46:40 +0000

    add confetti in service

--- src/app/game/components/happy-birthday/happy-birthday.ts
+++ src/app/game/components/happy-birthday/happy-birthday.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core'
+import { ConfettiService } from '../../../shared/services/confetti-service'
 
 @Component({
-  selector: 'app-happy-birthday',
-  standalone: true,
-  imports: [],
-  templateUrl: './happy-birthday.html',
-  styleUrl: './happy-birthday.css',
+    selector: 'app-happy-birthday',
+    standalone: true,
+    imports: [],
+    templateUrl: './happy-birthday.html',
+    styleUrl: './happy-birthday.css',
 })
-export class HappyBirthday {
+export class HappyBirthday implements OnInit, OnDestroy {
+    private _confettiService = inject(ConfettiService)
+    private intervalId: any
 
+    public ngOnInit(): void {
+        this._confettiService.shoot()
+        this.intervalId = setInterval(() => {
+            this._confettiService.shoot()
+        }, 3000)
+    }
+
+    public ngOnDestroy(): void {
+        if (this.intervalId) {
+            clearInterval(this.intervalId)
+        }
+    }
 }
