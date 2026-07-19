05c5ea9020dd0390cc49d42645b843d5f599f977
Federico González <federico1818@gmail.com>
2026-07-19 08:30:21 +0000

    item history

--- src/app/game/components/history-item/history-item.ts
+++ src/app/game/components/history-item/history-item.ts
@@ -2,10 +2,15 @@ import { Component, input } from '@angular/core'
 import { UserHistoryItem } from '../../models/user-history-item'
 import { TeamFlagComponent } from '../team-flag/team-flag'
 import { PointsBadge } from '../points-badge/points-badge'
+import { MatchPredictionBox } from '../match-prediction-box/match-prediction-box'
 
 @Component({
     selector: 'app-history-item',
-    imports: [TeamFlagComponent, PointsBadge],
+    imports: [
+        TeamFlagComponent,
+        PointsBadge,
+        MatchPredictionBox
+    ],
     templateUrl: './history-item.html',
     styleUrl: './history-item.css',
 })
