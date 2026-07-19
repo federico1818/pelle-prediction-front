ad6a18b3451e97751e4e670cd5da7e1e07de1b34
Federico González <federico1818@gmail.com>
2026-07-19 13:25:05 +0000

    show end

--- src/app/game/pages/home/home.ts
+++ src/app/game/pages/home/home.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core'
+import { Component, inject, OnInit, computed } from '@angular/core'
 import { News } from '../../components/news/news'
 import { MatchListUpcoming } from '../../components/match-list-upcoming/match-list-upcoming'
 import { NewsService } from '../../services/news-service'
+import { GamesService } from '../../services/games-service'
 import { End } from '../../components/end/end'
 
 @Component({
@@ -17,9 +18,19 @@ import { End } from '../../components/end/end'
 
 export class Home implements OnInit {
     private _newsService = inject(NewsService)
+    private _gamesService = inject(GamesService)
+
     public notices = this._newsService.notices
+
+    public gameFinished = computed(() => {
+        const finalGame = this._gamesService.playoffGames().find(
+            g => g.phase_name?.toLowerCase() === 'final'
+        )
+        return finalGame !== undefined && finalGame.ends_at !== null
+    })
 
     public ngOnInit(): void {
         this._newsService.get().subscribe()
+        this._gamesService.getPlayoffs().subscribe()
     }
 }
